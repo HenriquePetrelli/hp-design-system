2573c65efca8138d9d2e059266cbd9c85cbc5612
Henrique Petrelli <henriquepetrelli96@gmail.com>
2025-06-28 00:59:50 -0300

    refactor: retira tag de storybook ao copiar e edita nome do componente colocando prefixo Hp

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,12 @@ import type { Preview } from '@storybook/vue3'
 import 'virtual:svg-icons-register'
 import './preview.scss'
 import '@styles/style.scss'
+import * as components from '../src/components/index'
+
+// Extrai os nomes dos componentes removendo o prefixo "Hp"
+const componentNames = Object.keys(components).map((comp) =>
+  comp.replace(/^Hp/, '')
+)
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +16,29 @@ const preview: Preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i
+      }
+    },
+    docs: {
+      source: {
+        transform: (code: string) => {
+          let transformed = code
+            .replace(/^<template>\s*|\s*<\/template>$/g, '')
+            .trim()
+
+          // Aplica o replace para cada componente
+          componentNames.forEach((comp) => {
+            transformed = transformed
+              .replace(new RegExp(`<${comp}`, 'g'), `<Hp${comp}`)
+              .replace(new RegExp(`<\/${comp}`, 'g'), `<\/Hp${comp}`)
+          })
+
+          return transformed
+        }
+      }
+    },
+    options: {
+      storySort: {
+        method: 'alphabetical'
       }
     }
   }
