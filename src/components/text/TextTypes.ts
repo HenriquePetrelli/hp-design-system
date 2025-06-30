f1806b2f56e2437704538c0020ba3dd353759f66
Henrique Petrelli <henriquepetrelli96@gmail.com>
2025-06-30 01:25:48 -0300

    feat: criado types para componentes text

---
+++ src/components/text/TextTypes.ts
@@ -0,0 +1,17 @@
+export enum TextSize {
+  EXTRA_SMALL = 'xs',
+  SMALL = 'sm',
+  MEDIUM = 'md',
+  LARGE = 'lg',
+  EXTRA_LARGE = 'xl',
+  EXTRA_EXTRA_LARGE = 'xxl'
+}
+
+export enum HeadingLevel {
+  H1 = 1,
+  H2 = 2,
+  H3 = 3,
+  H4 = 4,
+  H5 = 5,
+  H6 = 6
+}
