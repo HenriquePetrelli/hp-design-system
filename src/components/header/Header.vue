684e949ff040d669769c5eddf9ecc21121770551
Henrique Petrelli <henriquepetrelli96@gmail.com>
2026-01-16 08:40:16 -0300

    feat: criados componentes Header e Sidebar

---
+++ src/components/header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <div class="header__left">
+      <slot name="header-left"></slot>
+    </div>
+
+    <div class="header__right">
+      <slot name="header-right"></slot>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss" src="./Header.scss" />
---
+++ src/components/sidebar/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+  <aside class="sidebar" :class="{ 'sidebar--open': open }">
+    <!-- Header slot -->
+    <div class="sidebar__header">
+      <slot name="sidebar-header"></slot>
+    </div>
+
+    <!-- Menu slot -->
+    <nav class="sidebar__nav">
+      <slot name="menu"></slot>
+    </nav>
+
+    <!-- Footer slot -->
+    <div class="sidebar__footer">
+      <slot name="sidebar-footer"></slot>
+    </div>
+  </aside>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  open: {
+    type: Boolean,
+    required: true
+  }
+})
+</script>
+
+<style scoped lang="scss" src="./Sidebar.scss" />
